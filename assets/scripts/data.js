f94d4c175a946a634ef5312349d143c10266b8e5
ashlenprice <ashlen.price@gmail.com>
2017-06-05 21:47:29 -0400

    Create loop for JSON data for each Election cycle
    create for in loops for the 08 & 12 election cycles
    saved parsed data to a variable and pushed data to an array
    
    changed array declarations from const to let.
    ** Total time generating the loop to manipulate the data 30 mins**
    
    Will try to calculate winner next and then try to map data to charts
    
    ** Time log per branch **
    forgot to add time spent on each branch in previous commit messages
    - in the planning and setup for commit 60 mins
    (researching JSON manipulation and how to parse, researching Charting
    libraries to display the data)
    - HTML Style  15 mins
    
    Total time remaining 2 hrs.
    
    - Assumption-
    Not factoring in commit msg drafting time

--- assets/scripts/data.js
+++ assets/scripts/data.js
@@ -1,31 +1,68 @@
-'use strict';
+'use strict'
 
 // imports parsed JSON file
-import cycleOne from '../data/results_2004.json';
-import cycleTwo from '../data/results_2008.json';
-import cycleThree from '../data/results_2012.json';
+import cycleOne from '../data/results_2004.json'
+import cycleTwo from '../data/results_2008.json'
+import cycleThree from '../data/results_2012.json'
 
 // empty object to push new data into
-const twoThousandAndFourCycle = new Object()
-const twoThousandAndEightCycle = []
-const twoThousandAndTwelveCycle = []
+let twoThousandAndFourCycle = []
+let twoThousandAndEightCycle = []
+let twoThousandAndTwelveCycle = []
 
+// 2004 cycle
 for (let state in cycleOne){
    if (cycleOne.hasOwnProperty(state)) {
-      //  console.log(cycleOne[state]);
+      //  console.log(cycleOne[state])
      for (let candidate in cycleOne[state]){
        if(cycleOne[state].hasOwnProperty(candidate)){
-              //  console.log(cycleOne[state][candidate]);
-         const electObj = cycleOne[state][candidate];
-        //  console.log(electObj);
-
-        //  twoThousandAndFourCycle.push(electObj);
-        //  console.log(twoThousandAndFourCycle);
+              //  console.log(cycleOne[state][candidate])
+let electObj = cycleOne[state][candidate]
+        //  console.log(electObj)
+         twoThousandAndFourCycle.push(electObj)
        }
          }
      }
  }
 
+ console.log(twoThousandAndFourCycle[2].name)
+
+//calculate winner per state
+
+
+// 2008 cycle
+ for (let state in cycleTwo){
+    if (cycleTwo.hasOwnProperty(state)) {
+       //  console.log(cycleTwo[state])
+      for (let candidate in cycleTwo[state]){
+        if(cycleTwo[state].hasOwnProperty(candidate)) {
+               //  console.log(cycleTwo[state][candidate])
+          let electObj = cycleTwo[state][candidate]
+         //  console.log(electResutObj)
+          twoThousandAndEightCycle.push(electObj)
+        }
+          }
+      }
+  }
+   console.log(twoThousandAndEightCycle[0])
+
+
+// 2012 cycle
+  for (let state in cycleThree){
+     if (cycleThree.hasOwnProperty(state)) {
+        //  console.log(cycleThree[state])
+       for (let candidate in cycleThree[state]){
+         if(cycleThree[state].hasOwnProperty(candidate)){
+                //  console.log(cycleThree[state][candidate])
+          let electObj = cycleThree[state][candidate]
+          //  console.log(electObj)
+           twoThousandAndTwelveCycle.push(electObj)
+         }
+           }
+       }
+   }
+
+    console.log(twoThousandAndTwelveCycle)
 
 
 
@@ -33,4 +70,4 @@ for (let state in cycleOne){
 
 
 
-module.exports = true;
+module.exports = true
