8d219ce5e07d0af6b0f86898b5ae72ae12756470
ashlenprice <ashlen.price@gmail.com>
2017-06-06 00:12:51 -0400

    Attempt to serialize form submission
    - created form, attempted to serialize but on submission the
    .serialize() method throws an error.
    - I am not familiar with serialization and I attempted to do some
    research on how to do that. I didn't find much and the little that I
    found was on the jquery method.
    
    In hindsight I, should have maybe started here or planned out some
    research time for new concepts and/or  asked for further clarification
    
    Moved form data into main container

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,12 +3,20 @@
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
 
+
 $(() => {
   setAPIOrigin(location, config);
 });
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example');
+// function to handle form submission and serialization
+const formSerializization = function () {
+  // this part of the function throws an error when active
+  //  Error may be due to .serialize() method
+  // $(".debug").text($("#myForm").serialize());
+  //     alert( "Thank you for your submission" );
+}
+$( ".btn" ).on( "submit", formSerializization );
+
 
 // use require without a reference to ensure a file is bundled
-require('./example');
+require('./maps');
